027223cc77a781f3e860c4e2a0ffef59770105cf
alex green <alxgreen@gmail.com>
2015-08-23 01:56:19 +0300

    Command Line Parser added

---
+++ DefragEngine/DefragEngine/CommandLineParser.cs
@@ -0,0 +1,50 @@
+﻿// Source: http://intellitect.com/converting-command-line-string-to-args-using-commandlinetoargvw-api/
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace DefragEngine
+{
+    public class CommandLineParser
+    {
+        [DllImport("shell32.dll", SetLastError = true)]
+        private static extern IntPtr CommandLineToArgvW(
+        [MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine, out int pNumArgs);
+
+        public static string[] CommandLineToArgs(string commandLine, out string executableName)
+        {
+            int argCount;
+            IntPtr result;
+            string arg;
+            IntPtr pStr;
+            result = CommandLineToArgvW(commandLine, out argCount);
+
+            if (result == IntPtr.Zero)
+            {
+                throw new System.ComponentModel.Win32Exception();
+            }
+            else
+            {
+                try
+                {
+                    pStr = Marshal.ReadIntPtr(result, 0 * IntPtr.Size);
+                    executableName = Marshal.PtrToStringUni(pStr);
+
+                    string[] args = new string[argCount - 1];
+                    for (int i = 0; i < args.Length; i++)
+                    {
+                        pStr = Marshal.ReadIntPtr(result, (i + 1) * IntPtr.Size);
+                        arg = Marshal.PtrToStringUni(pStr);
+                        args[i] = arg;
+                    }
+
+                    return args;
+                }
+                finally
+                {
+                    Marshal.FreeHGlobal(result);
+                }
+            }
+        }
+    }
+}
---
+++ DefragEngine/DefragEngineTests/CommandLineTests.cs
@@ -0,0 +1,24 @@
+﻿using DefragEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DefragEngineTests
+{
+    [TestClass]
+    public class CommandLineTests
+    {
+        [TestMethod]
+        public void CommandLineParseTest()
+        {
+            string procdumpTestLine = @"d:\sysinternals\procdump -n 10 -cpu 90";
+            string batTestLine = @"""d:\My Work\deploy_igloo_bin.bat"" ""d:\My Work\Interception\LowLevel\"" ";
+            string exec;
+            var args = CommandLineParser.CommandLineToArgs(procdumpTestLine, out exec);
+            Assert.AreEqual(@"d:\sysinternals\procdump", exec);
+            Assert.AreEqual(args.Length, 4);
+
+            args = CommandLineParser.CommandLineToArgs(batTestLine, out exec);
+            Assert.AreEqual(@"d:\My Work\deploy_igloo_bin.bat", exec);
+            Assert.AreEqual(args.Length, 1);
+        }
+    }
+}
